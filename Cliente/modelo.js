ba2ed181669fe4c6136a174679d1be61cad3222a
PedroJesusNavarroBlazquez <petrus_nvm@hotmail.com>
2020-10-20 20:17:51 +0200

    modificaciones Primera Entrega

--- Cliente/modelo.js
+++ Cliente/modelo.js
@@ -1,19 +1,24 @@
 function Juego(){
 	this.partidas={};
 	this.crearPartida=function(num,owner){
-		let codigo = this.obtenerCodigo();
-		if (!this.partidas[codigo]){
+		//Comprobamos los limites de num
+		let codigo="fallo";
+		if (!this.partidas[codigo] && this.numeroValido(num)){
+			codigo = this.obtenerCodigo();
 			this.partidas[codigo]=new Partida(num,owner.nick);
 			owner.partida=this.partidas[codigo];
-		}	
+		}else {
+			console.log(codigo);
+		}
 		return codigo;
 	}
+
 	this.unirAPartida=function(codigo,nick){
 		if(this.partidas[codigo]){
 			this.partidas[codigo].agregarUsuario(nick);
 		}
 	}
-	
+
 	this.obtenerCodigo=function(){
 		let cadena="ABCDEFGHIJKLMNOPQRSTUVWXZ";
 		let letras=cadena.split('');
@@ -24,13 +29,18 @@ function Juego(){
 		}
 		return codigo.join('');
 	}
+
+	this.numeroValido=function(num){
+		return (num>=4 && num <=10)
+	}
 }
 
 function Partida(num,owner){
 	this.maximo = num;
 	this.nickOwner = owner;
 	this.fase = new Inicial();
-	this.usuarios = {}//version array asociativa
+	this.usuarios ={}//version array asociativa
+	this.encargos=["tarea1", "tarea2", "tarea3","tarea4"];
 	this.agregarUsuario=function(nick){
 		this.fase.agregarUsuario(nick,this)
 	}
@@ -42,28 +52,47 @@ function Partida(num,owner){
 			contador=contador+1;
 		}
 		this.usuarios[nuevo] = new Usuario(nuevo);
-		this.usuario[nuevo].partida=this;
+		this.usuarios[nuevo].partida=this;
 		//this.comprobarMinimo();
 	}
 		this.comprobarMinimo=function(){
-		return Object.keys(this.usuarios).length>=4
+		return Object.keys(this.usuarios).length >= 4
+		}
+
+		this.comprobarMaximo=function(){
+			return Object.keys(this.usuarios).length < this.maximo
 		}
 
 		this.iniciarPartida=function(){
 			this.fase.iniciarPartida(this);
 		}
-		this.abandonarpartida=function(){
+
+		this.puedeIniciarPartida=function(){
+			this.fase=new Jugando();
+			this.asignarEncargos();
+			this.asignarImpostor();	
+		}
+
+		this.abandonarpartida=function(nick){
 			this.fase.abandonarpartida(nick,this);
 		}
 		this.eliminarUsuario=function(nick){
 			delete this.usuarios[nick];
 		}
 
+		this.asignarEncargos = function(){
+			//Asignaremos las tareas
+		}
+
+		this.asignarImpostor = function(){
+			//Asignaremos los impostores
+		}
+
 		this.agregarUsuario(owner);
 }
 
 function Inicial(){
-	this.nombre="Inicial";
+	this.nombre="inicial";
 	this.agregarUsuario=function(nick,partida){
 		partida.puedeAgregarUsuario(nick);
 		if (partida.comprobarMinimo()){
@@ -77,15 +106,23 @@ function Inicial(){
 		partida.eliminarUsuario(nick);
 		//comprobar si no quedan usuarios.
 	}
+
 }
 
 function Completado(){
 	this.nombre="completado";
 	this.iniciarPartida=function(partida){
 		
-		partida.fase= new Jugando();
+		//partida.fase= new Jugando();
+		partida.puedeIniciarPartida();
 	}
 	this.agregarUsuario=function(nick,partida){
+		if(partida.comprobarMaximo()){
+			partida.puedeAgregarUsuario(nick);
+		}
+		else{
+			console.log("Sorry, numero de usuario maximo");
+		}
 		console.log("La partida ya ha comenzado");
 	}
 	this.abandonarpartida=function(nick,partida){
@@ -130,6 +167,7 @@ function Usuario(nick,juego){
 	this.nick=nick;
 	this.juego=juego;
 	this.partida;
+	this.encargo="ninguno";
 	this.crearPartida=function(num){
 		return this.juego.crearPartida(num,this);
 	}
@@ -147,13 +185,14 @@ function randomInt(low, high) {
 
 function inicio(){
 	juego = new Juego();
-	var usr=new Usuario("pepe",juego);
-	var codigo=usr.crearPartida(4);
-
-	juego.unirAPartida(codigo,"luis");
-	juego.unirAPartida(codigo,"luisa");
-	juego.unirAPartida(codigo,"luisito");
-	juego.unirAPartida(codigo,"pepe");
+	var usr=new Usuario("pepe");
+	var codigo=juego.crearPartida(20,usr);
+	if(codigo!="fallo"){
+		juego.unirAPartida(codigo,"pedro");
+		juego.unirAPartida(codigo,"pedrito");
+		juego.unirAPartida(codigo,"luis");
+		//juego.unirAPartida(codigo,"pepe");
+	}
 
 	usr.iniciarPartida();
 
